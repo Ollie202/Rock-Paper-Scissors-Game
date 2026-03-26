8d475373670e2e32232a0f57275626fbbcbec3ec
Ollie202 <ollieweb3@gmail.com>
2026-03-26 15:38:54 +0100

    Update HTML, JS, and CSS with auto-play feature and button improvements

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -10,6 +10,52 @@ let score = JSON.parse(localStorage.getItem('score'));
 
   updateScoreElement();
 
+let isAutoPlaying = false;
+let intervalId;
+
+  function autoPlay() {
+    const autoPlayButton = document.querySelector('.auto-play-button');
+
+    if (!isAutoPlaying) {
+      intervalId = setInterval(() => {
+        const playerMove = pickComputerMove();
+        playGame(playerMove);
+      }, 1000);
+      isAutoPlaying = true;
+      autoPlayButton.innerHTML = 'Pause';
+
+    } else {
+      clearInterval(intervalId);
+      isAutoPlaying = false;
+      autoPlayButton.innerHTML = 'Auto Play';
+    }
+  }
+
+  document.querySelector('.js-rock-button')
+    .addEventListener('click', () => {
+      playGame('rock');
+    });
+
+  document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+      playGame('paper');
+    });
+
+  document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+      playGame('scissors');
+    });
+
+  document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+      playGame('rock');
+    } else if (event.key === 'p') {
+      playGame('paper');
+    } else if (event.key === 's') {
+      playGame('scissors');
+    }
+  });
+
   function playGame(playerMove) {
     const computerMove = pickComputerMove();
 
